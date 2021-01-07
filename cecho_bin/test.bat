98fb2d5ec52562b754aafd1cb182f0b94b260a7a
TheGamerz29 <66721586+TheGamerz29@users.noreply.github.com>
2021-01-07 11:56:11 +0800

    Add files via upload

---
+++ cecho_bin/test.bat
@@ -0,0 +1,14 @@
+@echo off
+color 0a
+mode 1500
+:disclaimer
+cls
+cecho                       {0a}Disclaimer{0d}:{#}{\n}
+cecho     {0f}This program is distributed for {0c}HOME {0f}use only.{#}{\n}
+cecho {0f}Piracy is {0c}bad{0f}. This program have got a {0d}special permission{#}{\n}
+cecho {0f}from the owner of this game: {0c}Bethesda®{0f}. An agreement from{#}{\n}
+cecho {0f}them that it's {0c}ILLEGAL {0f}to redistribute and playing this game{#}{\n}
+cecho      {0f}using {0d}QUIKE {0f}for {0c}creating content {0f}in any streaming or{#}{\n} 
+cecho     {0f}video sharing platform like {0d}Youtube or Twitch{#}{\n\t}
+echo to continue, you may press any key...
+pause>nul
